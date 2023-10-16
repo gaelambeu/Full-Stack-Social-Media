e1a421e166b60f263c0366180401a4370061fdd5
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-16 23:02:14 +0000

    update

--- server/models/commentModel.js
+++ server/models/commentModel.js
@@ -7,5 +7,17 @@ const commentSchema =  new mongoose.Schema(
         postId: { type: Schema.Types.ObjectId, ref: "Posts" },
         comment: { type: String, required: true },
         from: { type: String, required: true },
+        replies: [
+            {
+                rid: { type: mongoose.Schema.Types.ObjectId },
+                userId: { type: Schema.Types.ObjectId, ref: "Users" },
+                from: { type: String },
+                replyAt: { type: String },
+                comment: { type: String },
+                created_At: { type: Date, default: Date.now() },
+                updated_At: { type: Date, default: Date.now() },
+                likes: [{ type: String }],
+            }
+        ]
     }
 );
