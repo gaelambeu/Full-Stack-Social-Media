1a3bf1faa1c8d6940aac871105f3129a6b10cc21
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-16 22:48:31 +0000

    postMOdels

---
+++ server/models/postModel.js
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from  "mongoose";
+
+//schema
+const postSchema = new mongoose.Schema(
+    {
+        userId: { type: Schema.Types.ObjectId, ref: "Users" },
+        description: { type: String, required: true },
+        image: { type: String },
+        likes: [{ type: String }],
+        comments: [{ type: Schema.Types.ObjectId, ref: "Comments" }],
+    },
+    { timestamps }
+);
