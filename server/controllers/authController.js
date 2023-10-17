5457c84ab79df5a7a4e1adcd620c5692051d1f69
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-17 15:58:49 +0000

    short

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,4 +2,10 @@ import Users from "../models/userModel.js";
 
 export const register = async(req, res, next) => {
     const { firstName, lastName, email, password } = req.body;
+
+    //validate fileds
+    if (!(firstName || lastName || email || password)) {
+        next("Provide Required Fields!");
+        return;
+    }
 }
