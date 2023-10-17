5d582d169ff1a6a33d5fa0c323ffbc39c07b839c
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-17 16:15:27 +0000

    short

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,2 +1,9 @@
 import bcrypt from "bcryptjs";
 import JWT from "jsonwebtoken";
+
+export const hashString = async (useValue) => {
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedpassword = await bcrypt.hash(password, salt);
+    return hashedpassword;
+}
