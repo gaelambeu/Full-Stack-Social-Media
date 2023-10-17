d647eb9e19e016b4a46cfe2cb00addb7d0a80a66
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-17 16:34:20 +0000

    short

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,3 +1,11 @@
 import nodemailer from "nodemailer"
 import dotenv from "dotenv"
 import { v4 as uuidv4 } from "uuid"
+
+dotenv.config();
+
+const { AUTH_EMAIL, AUTH_PASSWORD, APP_URL } = process.env;
+
+let transporter = nodemailer.createTransport({
+    
+})
