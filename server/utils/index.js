6aee72f2c1c39f701e33afeef90ddd2fff2be45e
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-17 16:21:18 +0000

    json web token

--- server/utils/index.js
+++ server/utils/index.js
@@ -13,3 +13,10 @@ export const compareString = async (userPassword, password) => {
     const isMatch = await bcrypt.compare(userPassword, password);
     return isMatch;
 }
+
+//JSON WEBTOKEN
+export function createJWT(id) {
+    return JWT.sign({userId: id}, process.env.JWT_SECRET_KEY, {
+        expiresIn: "id"
+    })
+}
