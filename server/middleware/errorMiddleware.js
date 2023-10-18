d3efea90ea517d06255e097cd093ac54edf68901
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-18 19:46:54 +0000

    short

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -3,5 +3,17 @@
 const errorMiddleware = (err, req, res, next) => {
     const defaultError = {
         statusCode: 404,
+        success: "failed",
+        message: err,
+    };
+
+    if (err?.name === "ValidationError") {
+        defaultError.statusCode = 404;
+
+        defaultError.message = Object.values(err, errors)
+            .map((el) => el.message)
+            .join(",");
     }
+
+    //duplicate error
 }
