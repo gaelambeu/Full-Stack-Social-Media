7f636385ab6d716f75f763849922cbf8c81fef8b
GAEL AMBEU <ambeugael128@gmail.com>
2023-10-18 19:05:31 +0000

    short

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -27,13 +27,23 @@ export const sendverificationEmail = async (user, res) => {
         to: email,
         subject: "Email Verification",
         html:`<div
-        style='font-family: Arial, sans-serif; font-size: 20px; color: #333; background-color:'>
+        style='font-family: Arial, sans-serif; font-size: 20px; color: #333; background-color: #000'>
         <h1 style="color: rgb(8, 56, 188)">Please verify your email address</h1>
         <br>
         <h4>Hi, ${lastName}</h4>
         <p>
-            Please v
+            Please verify your email address so we can know that it's really you.
+            <br>
+            <p>This link <b>exprires in 1 hour</b></p>
+            <br>
+            <a href=${link}
+                style="color: #fff; padding: 14px; text-decoration: none; background-color: #000;"
+            > Email Address</a>
         </p>
+        <div style="margin-top: 20px;">
+            <h5>Best Regards</h5>
+            <h5>ShareFun Team</h5>
+        </div>
         </div>`
     }
 }
