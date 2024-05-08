75b95ceb39fffc890943883a01e7e9620ab588db
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-05-08 21:05:49 +0300

    short

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css'
+
 
 export default function App() {
   return (
-    <h1 className="w-full min-h-[100vh]">
-      p
-    </h1>
+    <div className="w-full min-h-[100vh]">
+      <p className="text-blue">Hello</p>
+    </div>
   )
 }
