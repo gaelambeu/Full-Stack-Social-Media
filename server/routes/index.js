c4004d1e2722855509771c688e6523955b027361
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-05-09 22:08:42 +0300

    short

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import authRoute from "./authRoutes.js"
-
-const router = express.Router();
-
-router.use(`/auth`, authRoute)    //auth/register
-
-export default router;
