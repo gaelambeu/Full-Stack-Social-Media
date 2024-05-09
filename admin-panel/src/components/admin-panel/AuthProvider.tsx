cef8473fcc6e1226e4f81a09b392716e43bbc127
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-05-09 23:25:02 +0300

    short

---
+++ admin-panel/src/components/admin-panel/AuthProvider.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { SessionProvider } from 'next-auth/react'
+import React from 'react'
+
+const AuthProvider = () => {
+  return (
+    <div>AuthProvider</div>
+  )
+}
+
+export default AuthProvider
