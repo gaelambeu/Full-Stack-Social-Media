000d6f9e6baf2e69c7fda5e56482284e7120974a
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-05-24 19:47:17 +0300

    short

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,19 @@ export default {
   ],
   theme: {
     colors:{
-      bgColor: "rgb(var(--color-bg) / <alpha-value>)"
+      bgColor: "rgb(var(--color-bg) / <alpha-value>)",
+      primary: "rgb(var(--color-primary) / <alpha-value>)",
+      secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+      blue: "rgb(var(--color-blue) / <alpha-value>)",
+      white: "rgb(var(--color-white) / <alpha-value>)",
+      ascent: {
+        1: "rgb(var(--color-ascent1) / <alpha-value>)",
+        2: "rgb(var(--color-ascent2) / <alpha-value>)",
+      },
+    },
+
+    screens: {
+      md: "768px"
     },
     extend: {},
   },
