5b2be826135e99e643ca070219e00f98a942fe53
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-05-25 19:11:25 +0300

    upadte

--- frontend/src/components/TextInput.jsx
+++ frontend/src/components/TextInput.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const TextInput = React.forwardRef (
             (
-                { type, placeholder, styles, label, labelStyles, register, name, error }
+                { type, placeholder, styles, label, labelStyles, register, name, error }, 
+                ref
             ) => {
         return (
             <div className="w-full flex flex-col mt-2 ">
@@ -15,8 +16,22 @@ const TextInput = React.forwardRef (
                 }
 
                 <div className="">
-                    <input type="text" />
+                    <input 
+                        type={type} 
+                        name={name}
+                        placeholder={placeholder}
+                        ref={ref}
+                        className={`bg-secondary rounded border border-[#66666690] 
+                                    outline-none text-sm text-ascent-1 px-4 py-3 
+                                    placeholder:text-[#666] ${styles}`
+                                }   
+                        {...register}   
+                        aria-invalid={error ? "true" : "false"}
+                    />
                 </div>
+                {error && (
+                    <span className="text-xs text-[#f64949fe] mt-0.5">{error}</span>
+                )}
             </div>
             
         )
