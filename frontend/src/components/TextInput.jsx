970b5ec3748b4f495cf54fac142fcf4eff369608
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-05-25 18:56:55 +0300

    update

--- frontend/src/components/TextInput.jsx
+++ frontend/src/components/TextInput.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 
-const TextInput = React.forwardRef (({
-    type, placeholder, styles, label, labelStyles, register, name, error
-}) => {
-  return (
-    <div>TextInput</div>
-  )
-})
+const TextInput = React.forwardRef (
+            (
+                { type, placeholder, styles, label, labelStyles, register, name, error }
+            ) => {
+        return (
+            <div className="w-full flex flex-col mt-2 ">
+                {label && 
+                    (
+                        <p className={`text-ascent-2 text-sm mb-2 ${labelStyles}`}>
+                            {label}
+                        </p>
+                    )
+                }
+
+                <div className="">
+                    <input type="text" />
+                </div>
+            </div>
+            
+        )
+    }
+)
 
 export default TextInput
