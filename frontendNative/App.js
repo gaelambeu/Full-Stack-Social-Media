89e53c5944f0e3678630b3a023fba23586e3e41f
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-06-03 23:35:13 +0300

    short

--- frontendNative/App.js
+++ frontendNative/App.js
@@ -1,20 +1,87 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Image, 
+  View, 
+  SafeAreaView, 
+  FlatList,
+  useWindowDimensions,
+  Text
+} from 'react-native';
+import data from './src/data/data.js'
 
-export default function App() {
+import Animated, {
+  useSharedValue,
+  useAnimatedScrollHandler,
+  useAnimatedRef,
+} from 'react-native-reanimated';
+
+
+const App = () => {
+  const {width: SCREEN_WIDTH} = useWindowDimensions();
+
+  const RenderItem = ({item, index}) => {
+    return (
+      <View style={[styles.itemContainer, {width: SCREEN_WIDTH}]}>
+        <Image source={item.image} style={{width: SCREEN_WIDTH * 0.8, height: SCREEN_WIDTH * 0.8}} />
+        <View>
+          <Text style={styles.itemTitle}>{item.title}</Text>
+          <Text style={styles.itemText}>{item.text}</Text>
+        </View>
+        
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <FlatList 
+        data={data} 
+        renderItem={({item, index}) => {
+          return <RenderItem item={item} index={index} />
+        }} 
+        keyExtractor={item => item.id}
+        scrollEventThrottle={16}
+        horizontal={true}
+        bounces={false}
+        pagingEnabled={true}
+        showsHorizontalScrollIndicator={false}
+      />
+      
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#63b8ea',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  itemContainer: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#63b8ea'
+  },
+
+  itemTitle: {
+    color: 'white',
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+
+  itemText: {
+    color: 'black',
+    textAlign: 'center',
+    lineHeight: 18,
+    marginHorizontal: 35,
+  }
 });
---
+++ frontendNative/src/data/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        id: 1,
+        image: require('../../src/assets/africa.png'),
+        title: 'lorem Ipsum',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    },
+    {
+        id: 2,
+        image: require('../../src/assets/panier.png'),
+        title: 'lorem Ipsum',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    },
+    {
+        id: 3,
+        image: require('../../src/assets/message.png'),
+        title: 'lorem Ipsum',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    }
+]
+
+export default data;
