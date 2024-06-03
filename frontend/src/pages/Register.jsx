e4c2bf1c7139973a707b6ba7ffeece5fd9a306e9
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-06-03 18:25:09 +0300

    update

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -13,6 +13,7 @@ const Register = () => {
   const {
     register, 
     handleSubmit, 
+    getValues,
     formState: { errors }
   } = useForm({
     mode: "onChange"
@@ -29,10 +30,10 @@ const Register = () => {
 
   return (
     <div className="bg-bgColor w-full h-[100vh] flex items-center justify-center p-6">
-      <div className="w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex flex-row-reverse bg-primary rounded-xl overflow-hidden shadow-xl">
+      <div className="flex flex-row-reverse w-full py-8 overflow-hidden shadow-xl md:w-2/3 h-fit lg:h-full 2xl:h-5/6 lg:py-0 bg-primary rounded-xl">
         
         {/* LEFT */}
-        <div className="w-full lg:w-1/2 h-full p-10 2xl:px-20 flex flex-col justify-center">
+        <div className="flex flex-col justify-center w-full h-full p-10 lg:w-1/2 2xl:px-20">
           <div className="flex items-center w-full gap-2 mb-6">
             <div className="p-2 bg-[#065ad8] rounded text-white">
               <TbSocial />
@@ -42,69 +43,112 @@ const Register = () => {
             </span>
           </div>
 
-          <p className="text-ascent-1 text-base font-semibold">
-            Log in to your account
+          <p className="text-base font-semibold text-ascent-1">
+              Create your account
           </p>
-          <span className="text-sm mt-2 text-ascent-2">Welcome back</span>
+          
 
-          <form onSubmit={handleSubmit(onSubmit)} className="py-8 flex flex-col gap-5">
-            <TextInput 
-              name='email'
-              placeholder='email@example.com'
-              label='email'
-              register={register("email", {
-                required: "Email Address is required"
-              })}
-              styles="w-full rounded-full"
-              labelStyle="ml-2"
-              error={errors.email ? errors.email.message : ""}
-            />
+          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5 py-8">
+            <div className="flex flex-col w-full gap-1 lg:flex-row md:gap-2">
+              <TextInput 
+                  name='firstName'
+                  label='First Name'
+                  placeholder='First Name'
+                  type='text'
+                  register={register("firstName", {
+                    required: "First Name is required"
+                  })}
+                  styles="w-full"
+                  error={errors.firstName ? errors.firstName.message : ""}
+                />
 
-            <TextInput 
-              name='password'
-              placeholder='*************'
-              label='password'
-              register={register("password", {
-                required: "Password is required"
-              })}
-              styles="w-full rounded-full"
-              labelStyle="ml-2"
-              error={errors.password ? errors.password.message : ""}
-            />
+                <TextInput 
+                  placeholder='Last Name'
+                  label='Last Name'
+                  register={register("lastName", {
+                    required: "Last do no match"
+                  })}
+                  styles="w-full"
+                  error={errors.lastName ? errors.lastName.message : ""}
+                />
+            </div>
+              
+            
+              <TextInput 
+                name='email'
+                placeholder='email@example.com'
+                label='email'
+                register={register("email", {
+                  required: "Email Address is required"
+                })}
+                styles="w-full"
+                error={errors.email ? errors.email.message : ""}
+              />
 
-            <Link
-              to='/reset-password'
-              className="text-sm text-right text-blue font-semibold"
-            >
-              Forgot Password?
-            </Link>
+              <div className="flex flex-col w-full gap-1 lg:flex-row md:gap-2">
+                <TextInput 
+                  name='password'
+                  placeholder='*************'
+                  label='Password'
+                  type='password'
+                  register={register("password", {
+                    required: "Password is required"
+                  })}
+                  styles="w-full"
+                  labelStyle="ml-2"
+                  error={errors.password ? errors.password.message : ""}
+                />
 
-            {
-              errMsg && (
-                <span className={`text-sm ${errMsg.status === "failed" ? "text-[#f64949fe]" : "text-[#2ba150fe]"} mt-0.5`}>
-                  {errMsg.message}
-                </span>
-              )
-            }
+                <TextInput 
+                  label='Confirm Password'
+                  placeholder='*************'
+                  type='password'
+                  register={register("cPassword", {
+                    validate: (value) => {
+                      const { password } = getValues();
+
+                      if (password != value) {
+                        return "Passwords do no match"
+                      }
+                    },
+                  })}
+                  styles="w-full"
+                  
+                  error={
+                    errors.cPassword && errors.cPassword.type === "validate" 
+                    ? errors.cPassword?.message
+                    :""
+                  }
+                />
+              </div>
+
+
+              {
+                errMsg && (
+                  <span className={`text-sm ${errMsg.status === "failed" ? "text-[#f64949fe]" : "text-[#2ba150fe]"} mt-0.5`}>
+                    {errMsg.message}
+                  </span>
+                )
+              }
 
             {
               isSubmitting ? <Loading /> : (
                 <CustomButton 
                   type="submit"
                   containerStyles="inline-flex justify-center rounded-md bg-blue px-8 py-2 text-sm font-medium text-white outline-none"
-                  title='Login'
+                  title='Create Account'
                 />
               )
             }
           </form> 
 
-          <p className="text-ascent-2 text-sm text-center">
-            Don't have an account?
+          <p className="text-sm text-center text-ascent-2">
+            Already has an account?{" "}
             <Link
-              to='/register'
+              to='/login'
               className='text-[#065ad8] font-semibold ml-2 cursor-pointer'
             >
-              Create Account
+              Login
             </Link>
           </p>
         </div>
@@ -117,32 +161,32 @@ const Register = () => {
         
         
         {/* RIGHT */}
-        <div className="hidden w-1/2 h-full lg:flex flex-col items-center justify-center bg-blue">
-          <div className="relative w-full flex items-center justify-center">
+        <div className="flex-col items-center justify-center hidden w-1/2 h-full lg:flex bg-blue">
+          <div className="relative flex items-center justify-center w-full">
             <img 
               src={BgImage} 
               alt="Bg Image" 
-              className='w-48 2xl:w-64 h-48 2xl:h-64 rounded-full object-cover'
+              className='object-cover w-48 h-48 rounded-full 2xl:w-64 2xl:h-64'
             />
 
-            <div className="absolute flex items-center gap-1 bg-white right-8 top-10 py-2 px-5 rounded-full">
+            <div className="absolute flex items-center gap-1 px-5 py-2 bg-white rounded-full right-8 top-10">
               <BsShare sire={14} />
               <span className="text-xs font-medium">Share</span>
             </div>
 
-            <div className="absolute flex items-center gap-1 bg-white left-6 top-6 py-2 px-5 rounded-full">
+            <div className="absolute flex items-center gap-1 px-5 py-2 bg-white rounded-full left-6 top-6">
               <ImConnection />
               <span className="text-xs font-medium">Connect</span>
             </div>
 
-            <div className="absolute flex items-center gap-1 bg-white left-6 bottom-6 py-2 px-5 rounded-full">
+            <div className="absolute flex items-center gap-1 px-5 py-2 bg-white rounded-full left-6 bottom-6">
               <AiOutlineInteraction />
               <span className="text-xs font-medium">Interact</span>
             </div>
           </div>
 
           <div className="mt-16 text-center">
-            <p className="text-white text-base">
+            <p className="text-base text-white">
               Connect with friends & have share for fun
             </p>
             <span className="text-sm text-white/80" />
