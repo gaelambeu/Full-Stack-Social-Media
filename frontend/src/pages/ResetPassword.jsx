ed36caf9a99f7eea937ab049c7fa2450e16e5265
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-06-03 19:11:46 +0300

    short

--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {useForm} from "react-hook-form";
 
 const ResetPassword = () => {
+  const [errMsg, setErrMsg] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+
+    formState: { errors },
+  } = useForm ({
+    mode: "onChange",
+  });
+
+  const onSubmit = async (data) => {};
+
   return (
-    <div>ResetPassword</div>
+    <div className="w-full h-[100vh] bg-bgColor flex items-center justify-center p-6">
+      <div className="bg-primary w-full md:w-1/3 2xl:w-1/4 px-6 py-8 shadow-md rounded-lg">
+        <p className="text-ascent-1 text-lg font-semibold">Email Address</p>
+
+        <span className="text-sm text-ascent-2">
+          Enter email address used during registration
+        </span>
+      </div>
+    </div>
   )
 }
 
