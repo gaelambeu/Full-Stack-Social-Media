86cd0bf1916fd19a4f6628d68b52797a8910c05c
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-06-03 19:26:09 +0300

    update

--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {useForm} from "react-hook-form";
+import {TextInput} from "../components"
 
 const ResetPassword = () => {
   const [errMsg, setErrMsg] = useState("");
@@ -24,6 +25,33 @@ const ResetPassword = () => {
         <span className="text-sm text-ascent-2">
           Enter email address used during registration
         </span>
+
+        <form onSubmit={handleSubmit(onsubmit)}
+        className="py-4 flex flex-col gap-5">
+          <TextInput 
+            name='email'
+            placeholder='email@example.com'
+            type='email'
+            regsiter={register("email", {
+              required: "Email Address is required",
+            })}
+            styles='w-full rounded-lg'
+            labelStyles='ml-2'
+            error={errors.email ? errors.email.message: ""}
+          />
+          {errMsg?.message && (
+            <span 
+            role='alert'
+            className={`text-sm ${
+              errMsg?.status === "failed"
+              ? "text-[#f64949fe]"
+              : "text-[#2ba150fe]"
+            } mt-0.5`}>
+              {errMsg?.message}
+            </span>
+          )}
+          
+        </form>
       </div>
     </div>
   )
