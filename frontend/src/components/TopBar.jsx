eda68357a7ac40800673b78a28a4fa2a4dfa1e38
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-06-06 17:51:55 +0300

    short

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -8,6 +8,8 @@ import { useForm } from 'react-hook-form'
 import { BsMoon, BsSunFill } from "react-icons/bs";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import {SetTheme} from "../redux/theme"
+import {Logout} from "../redux/userSlice"
+
 
 const TopBar = () => {
     const {theme} = useSelector((state) => state.theme)
@@ -62,6 +64,15 @@ const TopBar = () => {
             <button onClick={()=> handleTheme()}> {theme ? <BsMoon /> : <BsSunFill />} </button>
             <div className="hidden lg:flex">
                 <IoMdNotificationsOutline />
+            </div>
+
+            <div>
+                <CustomButton 
+                    onClick={() => dispatch(Logout())}
+                    title='Log Out'
+                    containerStyles='text-sm text-ascent-1 px-4 md:px-6 py-1 md:py-2
+                    border border-[#666] rounded-full' 
+                />
             </div>
         </div>
     </div>
