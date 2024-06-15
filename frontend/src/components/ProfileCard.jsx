04b0f37758a81c39c56cf029e2f76a7010962ac6
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-06-15 07:34:44 +0300

    upadta ProfileCard

--- frontend/src/components/ProfileCard.jsx
+++ frontend/src/components/ProfileCard.jsx
@@ -4,10 +4,11 @@ import { NoProfile } from '../assets';
 import { Link } from 'react-router-dom';
 import { UpdateProfile } from '../redux/userSlice';
 import { LiaEditSolid } from "react-icons/lia"
-import { BsBriefcase, BsPersonFillAdd } from 'react-icons/bs';
+import { BsBriefcase, BsFacebook, BsInstagram, BsPersonFillAdd } from 'react-icons/bs';
 import { CiLocationOn } from "react-icons/ci";
 
 import moment from "moment"
+import { FaTwitterSquare } from 'react-icons/fa';
 
 
 const ProfileCard = ({user}) => {
@@ -16,7 +17,7 @@ const ProfileCard = ({user}) => {
 
   return (
     <div>
-        <div className="w-full bg-primary flex flex-col items-center shadow-sm rounded-xl px-6 py-4">
+        <div className="flex flex-col items-center w-full px-6 py-4 shadow-sm bg-primary rounded-xl">
             <div className="w-full flex items-center justify-between border-b pb-5 border-[#66666645]">
                 <Link to={"/profile/" + user?._id} className="flex gap-2">
                     <img 
@@ -39,7 +40,7 @@ const ProfileCard = ({user}) => {
                     {user?._id === data?._id ? (
                         <LiaEditSolid 
                             size={22}
-                            className="text-blue cursor-pointer"
+                            className="cursor-pointer text-blue"
                             onClick={() => dispatch(UpdateProfile(true))}
                         />
                         
@@ -55,25 +56,25 @@ const ProfileCard = ({user}) => {
             </div>
 
             <div className="w-full flex flex-col gap-2 py-4 border-b border-[#66666645]">
-                <div className="flex gap-2 items-center text-ascent-2">
+                <div className="flex items-center gap-2 text-ascent-2">
                     <CiLocationOn className="text-xl text-ascent-1" />
                     <span>{user?.location ?? "Add Location"}</span>
                 </div>
 
-                <div className="flex gap-2 items-center text-ascent-2">
+                <div className="flex items-center gap-2 text-ascent-2">
                     <BsBriefcase className="text-lg text-ascent-1" />
                     <span>{user?.profession ?? "Add Profession"}</span>
                 </div>
             </div>
 
             <div className="w-full flex flex-col gap-2 py-4 border-b border-[#66666645]">
-                <p className="text-xl text-ascent-1 font-semibold">
+                <p className="text-xl font-semibold text-ascent-1">
                     {user?.friends?.length} Friends
                 </p>
 
                 <div className="flex items-center justify-between">
                     <span className="text-ascent-2">Who viewed your profile</span>
-                    <span className="text-ascent-1 text-lg">{user?.views?.length}</span>
+                    <span className="text-lg text-ascent-1">{user?.views?.length}</span>
                 </div>
 
                 <span className="text-base text-blue">
@@ -82,14 +83,29 @@ const ProfileCard = ({user}) => {
 
                 <div className="flex items-center justify-between">
                     <span className="text-ascent-2">Joined</span>
-                    <span className="text-ascent-1 text-base">
+                    <span className="text-base text-ascent-1">
                         {moment(user?.createdAt).fromNow()}
                     </span>
                 </div>
             </div>
 
-            <div className="w-full flex flex-col gap-4 py-4 pb-6">
-                <p className="text-ascent-1 text-lg font-semibold">Social Profile</p>
+            <div className="flex flex-col w-full gap-4 py-4 pb-6">
+                <p className="text-lg font-semibold text-ascent-1">Social Profile</p>
+                
+                <div className="flex items-center gap-2 text-ascent-2">
+                    <BsInstagram className="text-xl text-ascent-1" />
+                    <span>Instagram</span>
+                </div>
+
+                <div className="flex items-center gap-2 text-ascent-2">
+                    <FaTwitterSquare className="text-xl text-ascent-1" />
+                    <span>Twitter</span>
+                </div>
+
+                <div className="flex items-center gap-2 text-ascent-2">
+                    <BsFacebook className="text-xl text-ascent-1" />
+                    <span>Facebook</span>
+                </div>
             </div>
         </div>
     </div>
