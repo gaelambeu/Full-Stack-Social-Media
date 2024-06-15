a399d85fae8de44f819f4ea4659fb8ff0a196ece
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2024-06-15 07:59:48 +0300

    friends

--- frontend/src/components/FriendsCard.jsx
+++ frontend/src/components/FriendsCard.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
-const FriendsCard = () => {
+const FriendsCard = ({ friends }) => {
   return (
-    <div>FriendsCard</div>
+    <div>
+        <div className="w-full px-6 py-5 rounded-lg shadow-sm bg-primary">
+            <div className="flex items-center justify-between pb-2 border-b text-ascent-1 border-[#66666645]">
+                <span>Friends</span>
+                <span>{friends?.length}</span>
+            </div>
+
+            <div className="flex flex-col w-full gap-4 pt-4">
+            friends?.map((friend) => (
+                    <Link 
+                        to={"/profile/"+ friend?_id}
+                        key={friend?._id}
+                        className="flex items-center w-full gap-4 cursor-pointer"
+                    >
+                        <img  
+                            src={friend?.profileUrl ?? NoProfile} 
+                            alt={friend?.firstName} 
+                            className="object-cover w-10 h-10 rounded-full"
+                        />
+                    </Link>
+                ))
+            </div>
+        </div>
+    </div>
   )
 }
 
